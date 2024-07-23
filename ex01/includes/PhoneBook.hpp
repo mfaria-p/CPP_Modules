89b3e3d3d3fe0a4ebf0a5a51e14f51806afa2575
mfaria-p <mariana@Mariana>
2024-07-23 14:37:43 +0100

    started ex01

--- ex01/includes/PhoneBook.hpp
+++ ex01/includes/PhoneBook.hpp
@@ -1,5 +1,17 @@
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
+#include "Contact.hpp"
 
+class PhoneBook {
+    Contact contacts[8];
+    int n_contacts;
+    public:
+        PhoneBook () { //to inicialize the whole phonebook
+            n_contacts = 0;
+        }
+        void add_contact();
+        void print_all_contacts();
+        void search_contact();
+};
 #endif
---
+++ ex01/srcs/main.cpp
@@ -0,0 +1,18 @@
+#include "../includes/PhoneBook.hpp"
+#include "../includes/Contact.hpp"
+
+#include <iostream>
+
+int main(void)
+{
+    std::string cmd;
+    PhoneBook Phone;
+
+    std::cout << "The most incredible PhoneBoom you will ever see" << std::endl;
+    while (1)
+    {
+        std::cout << "enter a command: ";
+        std::cin >> cmd;
+    }
+
+}
