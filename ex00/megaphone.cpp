0951b2841b3cb466f607d39c666c1dc5be564bb2
Mariana Faria Pereira <mfaria-p@c1r7s5.42porto.com>
2024-08-30 17:23:22 +0100

    corrected megaphone

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,34 +1,23 @@
-#include <iostream>
-//to have the std functions
-void ft_upper(char *str)
+#include <iostream> //to have the std functions
+
+void    ft_toupper(std::string &word)
 {
-    int i;
-
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] >= 97 && str[i] <= 122)
-            str[i] -= 32;
-        std::cout << str[i];
-        i++;
-    }
-
+    int size = word.length();
+    for (int i = 0; i < size; i++)
+        word[i] = std::toupper(word[i]);
 }
+
 int main(int ac, char **av)
 {
-    int i;
+    std::string word;
 
-    i = 1;
     if (ac == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    for (int i = 1; i < ac; i++) 
     {
-        while (i < ac)
-        {
-            ft_upper(av[i]);
-            i++;
-        }
-        std::cout << std::endl; //to ensure the output is displayed
-        return (0);
+        word = (std::string)av[i];
+        ft_toupper(word);
+        std::cout << word;
     }
+    std::cout << std::endl;
 }
